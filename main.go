f331e077767f5c68ef117b864c69977672760f4f
vladimir vekhov <badcater@gmail.com>
2022-08-13 17:57:14 +0300

    gooo

--- main.go
+++ main.go
@@ -2,8 +2,17 @@ package main
 
 import "fmt"
 
-func main() {
+func print() {
 	fmt.Println("helllooo woorlldldddd")
 	fmt.Println(1)
 	fmt.Println(2 * 2)
+}
+func main() {
+	//message := "я есть я"
+	//  --------------------------------------------------------
+
+	var message string // = ("я есть я")
+	message = ("я есть я")
+
+	fmt.Println(message)
 }
