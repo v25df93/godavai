69b8858f81fe8f4e32e6033c1890727413b8bc3b
vladimir vekhov <badcater@gmail.com>
2022-08-14 14:11:51 +0300

    zdarova21

--- main.go
+++ main.go
@@ -8,11 +8,13 @@ func printMessage(message string) {
 	fmt.Println(message)
 }
 
-func sayhello(name string) string {
-	return "привет, " + name + "!!!"
+func sayhello(name string, age int) string {
+	return fmt.Sprintf("Привет, %s! Тебе %d лет!", name, age)
 }
 
 func main() {
-	message := sayhello("Максим")
+	var message string
+	message = sayhello("Максим", 21)
+
 	printMessage(message)
 }
