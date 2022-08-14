f1b8c3d10de9ac22bb7a5c8fd175a080a86c07f2
vladimir vekhov <badcater@gmail.com>
2022-08-14 23:38:36 +0300

    anonimnaya_func

--- main.go
+++ main.go
@@ -1,24 +1,12 @@
 package main
 
-import (
-	f "fmt"
-)
+import "fmt"
 
 func main() {
-	f.Println(findmax(1, 2, 5, 7, 788, 535, 321, -2, -01, -55, 2527))
-}
-
-func findmax(numbers ...int) int {
-	if len(numbers) == 0 {
-		return 0
+	call := func(name string) {
+		fmt.Println("Calling", name)
 	}
-	max := numbers[0]
-
-	for _, i := range numbers {
-		if i > max {
-			max = i
-		}
-	}
-
-	return max
+	call("Meet")
+	person := "Chris"
+	call(person)
 }
