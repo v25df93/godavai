1883986ff089caba95db72311deb750d5c973048
vladimir vekhov <badcater@gmail.com>
2022-08-14 20:08:09 +0300

    errorsclub

--- main.go
+++ main.go
@@ -1,20 +1,32 @@
 package main
 
 import (
+	"errors"
 	"fmt"
+	"log"
 )
 
-func enterTheClub(age int) (string, bool) {
-	if age >= 18 {
-		return "Входи.", true
-	} else {
-		return "Тебе нет 18.", false
+func enterTheClub(age int) (string, error) {
+	if age >= 18 && age <= 45 {
+		return "Входи.", nil
+	} else if age >= 45 && age < 65 {
+		return "Вам точно понравится?", nil
+	} else if age > 65 {
+		return "Это уже слишком", errors.New("you are too old")
 	}
+
+	return "Нельзя", errors.New("you are too young")
+
 }
 
 func main() {
-	message, entered := enterTheClub(18)
-	fmt.Println(message)
-	fmt.Println(entered)
+	message, err := enterTheClub(12)
 
+	if err != nil {
+		log.Fatal(err)
+		fmt.Println(err)
+		return
+	}
+
+	fmt.Println(message)
 }
