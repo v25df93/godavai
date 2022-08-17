be96f9ddf690afeb4fdd07b0d20eed5270904ad4
vladimir vekhov <badcater@gmail.com>
2022-08-17 17:16:29 +0300

    2e_declare

--- main.go
+++ main.go
@@ -2,14 +2,11 @@ package main
 
 import "fmt"
 
-var msg string
-
-func init() {
-
-	msg = "cum"
-
-}
-
 func main() {
-	fmt.Println(msg)
+	var magicNum, powerLevel int32
+	magicNum = 2048
+	powerLevel = 9001
+	fmt.Println("magicNum is:", magicNum, "powerLevel is:", powerLevel)
+	amount, unit := 10, "doll hairs"
+	fmt.Println(amount, unit, ", that's expensive...")
 }
