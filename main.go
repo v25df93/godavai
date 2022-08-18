2be90fc134236ad88edbb87505bfdd984c0b6a82
vladimir vekhov <badcater@gmail.com>
2022-08-18 18:05:05 +0300

    X_OR_VAR_SWAP

--- main.go
+++ main.go
@@ -1,52 +1,16 @@
 package main
 
-import (
-	"fmt"
-	"math/rand"
-	"time"
-)
+import "fmt"
+
+func swap(a int, b int) {
+	a ^= b
+	b ^= a
+	a ^= b
+	fmt.Println(a, b)
+}
 
 func main() {
-	rand.Seed(time.Now().UnixNano())
-	isHeistOn := true
-	eludedGuards := rand.Intn(100)
-	if eludedGuards <= 50 {
-		fmt.Println("Looks like you've managed to make it past the guards. Good job, but remember, this is the first step.")
-	} else {
-		isHeistOn = false
-		fmt.Println("Plan a better disguise next time?")
-	}
-	fmt.Println(isHeistOn)
-
-	openedVault := rand.Intn(100)
-	if isHeistOn && openedVault >= 70 {
-		fmt.Println("Grab and GO!")
-	} else if isHeistOn && openedVault < 70 {
-		isHeistOn = false
-		fmt.Println("Vault can't be opened!")
-	}
-
-	leftSafely := rand.Intn(5)
-	if isHeistOn {
-		switch leftSafely {
-		case 0:
-			isHeistOn = false
-			fmt.Println("Failed heist")
-		case 1:
-			isHeistOn = false
-			fmt.Println("Failed heist")
-		case 2:
-			isHeistOn = false
-			fmt.Println("Failed heist")
-		case 3:
-			isHeistOn = false
-			fmt.Println("Failed heist")
-		default:
-			fmt.Println("Start the getaway car!")
-		}
-		if isHeistOn {
-			amtStolen := 1000 + rand.Intn(1000000)
-			fmt.Println("$", amtStolen, " WE GOT!!!")
-		}
-	}
+	a := 10
+	b := 4
+	swap(a, b)
 }
