4b37801fe93d6d4ac59418829468b7281300935d
vladimir vekhov <badcater@gmail.com>
2022-08-18 17:27:32 +0300

    BANK_HEIST_LOGIC

--- main.go
+++ main.go
@@ -2,17 +2,51 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
-	i := "davai govno"
-	fmt.Scan(&i)
-	switch i {
-	case "if":
-		fmt.Println("if > else")
-	case "else":
-		fmt.Println("net epta ina4e")
-	default:
-		fmt.Println("GEIII")
+	rand.Seed(time.Now().UnixNano())
+	isHeistOn := true
+	eludedGuards := rand.Intn(100)
+	if eludedGuards <= 50 {
+		fmt.Println("Looks like you've managed to make it past the guards. Good job, but remember, this is the first step.")
+	} else {
+		isHeistOn = false
+		fmt.Println("Plan a better disguise next time?")
+	}
+	fmt.Println(isHeistOn)
+
+	openedVault := rand.Intn(100)
+	if isHeistOn && openedVault >= 70 {
+		fmt.Println("Grab and GO!")
+	} else if isHeistOn && openedVault < 70 {
+		isHeistOn = false
+		fmt.Println("Vault can't be opened!")
+	}
+
+	leftSafely := rand.Intn(5)
+	if isHeistOn {
+		switch leftSafely {
+		case 0:
+			isHeistOn = false
+			fmt.Println("Failed heist")
+		case 1:
+			isHeistOn = false
+			fmt.Println("Failed heist")
+		case 2:
+			isHeistOn = false
+			fmt.Println("Failed heist")
+		case 3:
+			isHeistOn = false
+			fmt.Println("Failed heist")
+		default:
+			fmt.Println("Start the getaway car!")
+		}
+		if isHeistOn {
+			amtStolen := 1000 + rand.Intn(1000000)
+			fmt.Println("$", amtStolen, " WE GOT!!!")
+		}
 	}
 }
