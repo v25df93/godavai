68ada62cc48156212f42861c3dfe45d92df45fc8
vladimir vekhov <badcater@gmail.com>
2022-08-22 18:08:36 +0300

    added folder

--- main.go
+++ main.go
@@ -1,14 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
-func joinTwoStrings(prefix string, suffix string) string {
-	fmt.Println(prefix, suffix)
-	return prefix + suffix
-}
-
 func main() {
-	joinTwoStrings("vasil'ev", "artem")
+
 }
